069685891a5dd99db175459cab486580b8bd2ee3
kenVS <yangyang280888@hotmail.com>
2020-07-08 00:54:17 +0800

    added new functions

--- MathLib/MathLib.cpp
+++ MathLib/MathLib.cpp
@@ -9,3 +9,13 @@ int MathLib::subtract(int a, int b)
 {
 	return a - b;
 }
+
+int MathLib::multiply(int a, int b)
+{
+	return a * b;
+}
+
+int MathLib::divide(int a, int b)
+{
+	return a / b;
+}
--- MathLib/MathLib.h
+++ MathLib/MathLib.h
@@ -5,4 +5,6 @@ class MathLib
 public:
 	static int add(int a, int b);
 	static int subtract(int a, int b);
+	static int multiply(int a, int b);
+	static int divide(int a, int b);
 };
